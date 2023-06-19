02bf2fd3dc08afd6ef4c6c09a0a1c0e2f5c79398
sung <48851179+naturepoker@users.noreply.github.com>
2023-06-19 13:34:28 -0400

    adding quick muscle-trim-tree script

---
+++ muscle_trim_fasttree.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if ! command -v muscle > /dev/null; then
+        printf "\n Muscle aligner not found in path. Exiting"
+        exit
+fi
+
+if ! command -v trimal > /dev/null; then
+        printf "\n Trimal not found in path. Exiting"
+        exit
+fi
+
+align_input=$(echo $1 | tr -d '\r')
+alignment_name=$(basename $1 .faa)
+
+muscle -align "$align_input" --output "$alignment_name".afa
+
+trimal -automated1 -in "$alignment_name".afa -out autotrim_"$alignment_name".afa
+
+FastTreeDbl -lg -slow autotrim_"$alignment_name".afa > fasttree_"$alignment_name".tree
